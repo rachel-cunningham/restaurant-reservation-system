a9b60ce5256117dd26acf2a657526ac76d2f7268
rachel-cunningham <123897610+rachel-cunningham@users.noreply.github.com>
2023-07-23 19:12:45 -0400

    Update reservations.service.js

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -7,14 +7,29 @@ function list() {
 function listByDate(date) {
   return knex("reservations").select("*").where({ reservation_date: date });
 }
+function listByMobileNumber(mobile_number){
+  return knex("reservations").select("*").where({ mobile_number: mobile_number });
+}
+function reservationById(reservation_id) {
+  return knex("reservations").select("*").where({ reservation_id: reservation_id });
+}
 function create(reservation) {
   return knex("reservations")
     .insert(reservation)
     .returning("*")
     .then((createdRecords) => createdRecords[0]);
 }
+function update(reservation_id,data) {
+  return knex("reservations")
+      .where({ reservation_id: reservation_id })
+      .update(data)
+      .select("*");
+}
 module.exports = {
   list,
   listByDate,
-  create
+  create,
+  update,
+  reservationById,
+  listByMobileNumber
 };
