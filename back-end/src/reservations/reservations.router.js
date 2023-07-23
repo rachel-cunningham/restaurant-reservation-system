1513198139e29b9576e8527e0d2871fbcf5a487d
rachel-cunningham <123897610+rachel-cunningham@users.noreply.github.com>
2023-07-23 19:12:20 -0400

    Update reservations.router.js

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
-
+router.route("/:reservation_id/status").put(controller.update);
 router.route("/").get(controller.list).post(controller.create);
 
 module.exports = router;
