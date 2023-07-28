835a9ad35384a03e4d3c30b1254d9a32bdb127dd
rachel-cunningham <123897610+rachel-cunningham@users.noreply.github.com>
2023-07-28 16:27:14 -0400

    Update ReservationsNew.js

--- front-end/src/reservations/ReservationsNew.js
+++ front-end/src/reservations/ReservationsNew.js
@@ -1,132 +1,160 @@
 import { React, useState } from "react";
 import { useHistory } from "react-router-dom/";
+import { createReservation } from "../utils/api";
 import { today } from "../utils/date-time";
-
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import MuiPhoneNumber from 'material-ui-phone-number';
+import "./ReservationNew.css"
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import resimage from './../images/my-res.jpeg';
+import Alert from '@mui/material/Alert';
 function NewReservation() {
   const history = useHistory();
+  const [reservationsError, setReservationsError] = useState(null);
+
+  const goBack = () => {
+    history.goBack(); 
+  }
+
+  const goToReservation = () => {
+    history.push(`/dashboard?date=${formData.reservation_date}`)
+  }
+
+  
   const initialFormState = {
     first_name: "",
     last_name: "",
     mobile_number: "",
-    reservation_date: "",
+    reservation_date: today(),
     reservation_time: "",
-    numberOfPeople: "",
+    people: 1,
+    status:"booked"
   };
 
   const [formData, setFormData] = useState({ ...initialFormState });
   const handleChange = ({ target }) => {
+    console.log(target);
+    if(target){
+      setFormData({
+        ...formData,
+        [target.name]: target.value,
+      });      
+    }
+  };
+  function handlePhone(value) {
     setFormData({
       ...formData,
-      [target.name]: target.value,
+      'mobile_number': value,
     });
-  };
-
+ }
+ function handleDate(value) {
+  setFormData({
+    ...formData,
+    'reservation_date': dayjs(value).format('YYYY-MM-DD'),
+  });
+}
+function handleTime(value) {
+  console.log(dayjs(value).format('HH:mm'));
+  setFormData({
+    ...formData,
+    'reservation_time': dayjs(value).format('HH:mm'),
+  });
+}
   const handleSubmit = async (event) => {
     event.preventDefault();
-    history.push(`/dashboard/${formData.reservation_date}`);
+    const abortController = new AbortController();
+    createReservation({data:formData}, abortController.signal)
+    .then((resp)=>{
+      console.log(resp);
+      goToReservation();
+    } )
+    .catch((err)=>{
+      console.log(err.message);
+      setReservationsError(err.message);
+    })
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="first_name">
-            First Name:{" "}
-            <input
-              className="form-style"
-              id="first_name"
-              type="text"
-              name="first_name"
-              onChange={handleChange}
-              required={true}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="last_name">
-            Last Name:{" "}
-            <input
-              className="form-style"
-              id="last_name"
-              type="text"
-              name="last_name"
-              onChange={handleChange}
-              required={true}
-            />
-          </label>
-          <div>
-            <label htmlFor="mobile_number">
-              Moblie Number:{" "}
-              <input
-                className="form-style"
-                id="mobile_number"
-                type="tel"
-                name="mobile_number"
-                onChange={handleChange}
-                placeholder="xxx-xxx-xxxx"
-                required={true}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="reservation_date">
-              Date of Reservation:{" "}
-              <input
-                className="form-style"
-                id="reservation_date"
-                type="date"
-                name="reservation_date"
-                pattern="\d{4}-\d{2}-\d{2}"
-                min={today}
-                max="2050-12-31"
-                onChange={handleChange}
-                required={true}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="reservation_time">
-              Time of Reservation:{" "}
-              <input
-                className="form-style"
-                id="reservation_time"
-                type="time"
-                name="reservation_time"
-                pattern="[0-9]{2}:[0-9]{2}"
-                onChange={handleChange}
-                required={true}
-              />
-            </label>
-          </div>
-        </div>
-        <div>
-          <label htmlFor="people">
-            Party Size:{" "}
-            <input
-              className="form-style"
-              id="people"
-              type="number"
-              min="1"
-              name="people"
-              placeholder="1"
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div class="c-buttons">
-          <button
-            name="cancel"
-            className="btn btn-secondary mr-1"
-            onClick={() => history.push(`/`)}
-          >
-            Cancel
-          </button>
-          <div className="divider"> </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-      </form>
+      <h1>Create Reservation</h1>
+      <Stack spacing={2} sx={{marginBottom: 4}} direction={{ xs: 'column', sm: 'column',md:'column',lg:'row',xl:'row' }}>        
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={2} direction="row" sx={{marginBottom: 4}}>
+                <TextField
+                          type="text"
+                          variant='outlined'
+                          color='secondary'
+                          label="First Name"
+                          name="first_name"
+                          onChange={handleChange}
+                          fullWidth
+                          required
+                      />
+                      <TextField
+                          type="text"
+                          variant='outlined'
+                          color='secondary'
+                          label="Last Name"
+                          name="last_name"
+                          onChange={handleChange}
+                          fullWidth
+                          required
+                      />
+          </Stack>
+          <Stack spacing={2} direction="row" sx={{marginBottom: 4}}>
+              <MuiPhoneNumber defaultCountry={'us'} onChange={handlePhone} required fullWidth></MuiPhoneNumber>
+          </Stack>
+          <Stack spacing={2} direction="row" sx={{marginBottom: 4}} useFlexGap>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Reservation Date"
+                  onChange={(newValue) => handleDate(newValue)}
+                  slotProps={{
+                    textField: {
+                      helperText: 'MM/DD/YYYY',
+                    },
+                  }}
+                />
+              </LocalizationProvider>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <TimePicker label="Reservation Time"
+            onChange={(newValue) => handleTime(newValue)}
+          />
+              </LocalizationProvider>
+          </Stack>
+          <Stack spacing={2} direction="row" sx={{marginBottom: 4}}>
+          <TextField
+            id="people"
+            label="Party Size"
+            type="number"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+            onChange={handleChange}
+            min= "1"
+            name="people"
+            placeholder="1"
+            fullWidth
+          />
+          </Stack>
+            <Stack spacing={2} direction="row" justifyContent="flex-end">
+            <Button variant="outlined" onClick={goBack} size="large">Cancel</Button>
+            <Button variant="contained" type="submit" size="large">Submit</Button>
+            </Stack>  
+            {reservationsError &&<Alert severity="error">{reservationsError}</Alert>}        
+        </form>
+        <img src={resimage} alt="logo" />
+      </Stack>
+      <div>
+        
+      </div>
     </div>
   );
 }
