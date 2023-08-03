d523ba53b1fee7b039f35388c9d8aa27f262877e
rachel-cunningham <123897610+rachel-cunningham@users.noreply.github.com>
2023-08-03 10:19:26 -0400

    Add files via upload

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import Tables from "../tables/Tables";
 import "./Dashboard.css";
 import Reservations from "./Reservations";
 import ReservationsNav from "./ReservationsNav";
-
+import Stack from '@mui/material/Stack';
 /**
  * Defines the dashboard page.
  * @param date
@@ -24,8 +25,16 @@ function Dashboard({ date }){
   },[])
   return (
     <main>  
-      <h1>Reservations for {displayDate}</h1>
-      <Reservations keyString='date' value={displayDate}></Reservations>
+      <Stack spacing={2} direction="row">
+        <div className="res-col">
+            <h3 className="header-res">Reservations for {displayDate}</h3>
+            <Reservations keyString='date' value={displayDate}></Reservations>
+        </div>
+        <div className="table-col">
+            <h3 className="header-table">Tables</h3>
+            <Tables></Tables>
+        </div>
+      </Stack>
       <ReservationsNav date={displayDate} onDateChange={handleDate}></ReservationsNav>
     </main>
   );
--- front-end/src/dashboard/Reservations.js
+++ front-end/src/dashboard/Reservations.js
@@ -6,12 +6,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { listReservations, updateReservationStatus } from '../utils/api';
 import "./ReservationsNav.css"
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import "./Reservation.css"
 
 export default function Reservations({keyString,value}) {
+    const history = useHistory();
     const [reservations, setReservations] = useState([]);
     const [reservationsError, setReservationsError] = useState(null);
     useEffect(loadDashboard, [keyString, value]);
@@ -43,48 +46,43 @@ export default function Reservations({keyString,value}) {
         .catch((err)=>{
             console.log(err.message);
         })
+        history.push(`/reservations/1/seat`);
     }
   return (
     <div>
-        {reservationsError && reservationsError}
+        {reservationsError}
         <div>{Array.isArray(reservations) && reservations.length>0 &&
-            <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>First Name</TableCell>
-                    <TableCell align="left">Last Name</TableCell>
-                    <TableCell align="left">Mobile Number</TableCell>
-                    <TableCell align="left">Reservation Time</TableCell>
-                    <TableCell align="left">People</TableCell>
-                    <TableCell align="left">Status</TableCell>
-                    <TableCell align="left"></TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {reservations.map((row) => (
-                    <TableRow>
-                    <TableCell align='left'>
-                        {row.first_name}
-                    </TableCell>
-                    <TableCell align="left">{row.last_name}</TableCell>
-                    <TableCell align="left">{row.mobile_number}</TableCell>
-                    <TableCell align="left">{row.reservation_time}</TableCell>
-                    <TableCell align="left">{row.people}</TableCell>
-                    <TableCell align="left">                        
-                            {row.status}                        
-                    </TableCell>
-                    <TableCell align="left">                        
-                        <Stack direction="row" spacing={2} alignItems='left'>                            
-                            {row.status?.toLowerCase()==='booked' && <Button variant="contained" onClick={markSeated(row.reservation_id)}>Seat</Button>}
-                            {row.status?.toLowerCase()!=='booked' && <Button disabled>Seated</Button>}                            
+            reservations.map((reservation) => (
+                <div className='main-card'>
+                    <div className='card-res'>
+                        <div className='info'>
+                            <div className='name-wrapper'>
+                                <h3 className='name'>{reservation.first_name}</h3>
+                                <h3 className='name'>{reservation.last_name}</h3>
+                            </div>
+                            
+                        </div>
+                        <div className='subinfo-res'>
+                            <div className='people'>Party of {reservation.people}</div>
+                            <div className='desc-at'>At: {reservation.reservation_time}</div>
+                        </div>
+                        <div className='subinfo-res'>
+                            <div className='desc-ct'>mobile: {reservation.mobile_number}</div>
+                            {reservation.status?.toLowerCase()!=='seated' && <div className='people'>{reservation.status.toUpperCase()}</div>}
+                        </div>    
+                    </div>
+                    <div className='buttons'>
+                        <Stack direction="column" spacing={2} alignItems='left'>                            
+                            {reservation.status?.toLowerCase()==='booked' && <Button variant="contained" onClick={markSeated(reservation.reservation_id)}>Seat</Button>}
+                            {reservation.status?.toLowerCase()!=='booked' && <Button disabled>Seated</Button>}
+                            <Button variant="contained">Edit</Button>
+                            <Button variant="outlined">Cancel</Button>                            
                         </Stack>
-                    </TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-            </TableContainer>}
+                    </div>
+                </div>
+            ))
+            
+            }
         </div>
         
     </div>
