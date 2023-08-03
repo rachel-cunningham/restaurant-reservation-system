e7a54eac6adfbdd54cce7b8a2122b357a25a6cab
rachel-cunningham <123897610+rachel-cunningham@users.noreply.github.com>
2023-08-03 10:15:35 -0400

    Delete tables.router.js

--- front-end/src/tables/tables.router.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const controller = require("./tables.controller");
-router.route("/:table_id/seat").put(controller.reserveTable).delete(controller.remove);
-router.route("/").get(controller.list).post(controller.create);
-
-module.exports = router;
